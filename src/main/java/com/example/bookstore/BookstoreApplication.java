16a860012ba4c8c725d8136b7a36b980a60ca989
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-02-12 18:28:27 +0200

    Added edit, save and delete method

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway",1929, "1232323-21", 20.00));
+			repository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 50.00));	
+			//repository.deleteAll();
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
 }
+
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,11 +1,52 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
+
+@Controller
 public class BookController {
-	@RequestMapping("/index")
+	
+	@Autowired
+	BookRepository repository; 
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookstore(Model model) {
+		
+		model.addAttribute("book", new Book());
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+    @RequestMapping(value = "/addbook")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }    
+    
+    @RequestMapping(value = "/editbook/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    	model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    }
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }     
+}
 	
 		
-}
