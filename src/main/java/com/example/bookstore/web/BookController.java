a0a071e1d47e458ec370bbdbac05de9251b5ab0a
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-04-02 19:42:39 +0300

    Added test cases

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -89,7 +89,7 @@ public class BookController {
     
     @RequestMapping(value="/logout", method = RequestMethod.GET)
     public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        org.springframework.security.core.Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null){    
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
